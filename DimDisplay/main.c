57214ffc0b4081ba38c7e183f4e346ca509e66b9
Stanisław Uss <stanislawuss@Stanisaw-Usss-iMac.local>
2016-02-24 20:58:40 +0100

    Init

--- DimDisplay/main.c
+++ DimDisplay/main.c
@@ -6,10 +6,13 @@
 //  Copyright © 2016 Stanisław Uss. All rights reserved.
 //
 
-#include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(void) {
+    io_registry_entry_t r = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/IOResources/IODisplayWrangler");
+    if(!r) return 1;
+    int err = IORegistryEntrySetCFProperty(r, CFSTR("IORequestIdle"), kCFBooleanTrue);
+    IOObjectRelease(r);
+    return err;
 }
